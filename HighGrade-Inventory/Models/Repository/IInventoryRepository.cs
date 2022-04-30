8419eeafca3e25e0b33e6e2670158669b2e39263
iamstan13y <iamstan13y@gmail.com>
2022-04-30 10:37:56 +0200

    Inventory Repository Interface.

---
+++ HighGrade-Inventory/Models/Repository/IInventoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using HighGradeInventory.API.Models.Data;
+using HighGradeInventory.API.Models.Local;
+
+namespace HighGradeInventory.API.Models.Repository
+{
+    public interface IInventoryRepository
+    {
+        Task<Result<IEnumerable<Inventory>>> GetAllAsync();
+        Task<Result<Inventory>> GetByIdAsync(int id);
+        Task<Result<Inventory>> AddAsync(Inventory inventory);
+    }
+}
