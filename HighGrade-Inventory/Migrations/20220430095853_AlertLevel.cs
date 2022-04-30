77092216f6694b9598a1ea9abdb7aacf2c7d46be
iamstan13y <iamstan13y@gmail.com>
2022-04-30 12:00:44 +0200

    Alert Level DB Migrations.

---
+++ HighGrade-Inventory/Migrations/20220430095853_AlertLevel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HighGradeInventory.API.Migrations
+{
+    public partial class AlertLevel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AlertLevel",
+                table: "Inventories",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlertLevel",
+                table: "Inventories");
+        }
+    }
+}
